ed0002c828fb690d9104d92658f2554fc6b72669
Lucas Mbiwe <lucas@geek4good.com>
2019-03-27 13:36:03 +0000

    Sketch the endpoint's basic functionality
    Create a draft of the different phases the endpoint goes through:
      - basic error handling
      - input processing
      - polynomial construction
      - returning a value

--- derivatives.rb
+++ derivatives.rb
@@ -1,6 +1,18 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 
-get "/" do
-  "Hello world!"
+get "/differentiate/*" do
+  coefficients = parse_coefficients!(params[:splat].first) or return
+
+  polynomials = coefficients.map.with_index(1) do |coefficient, position|
+    Polynomial.new(coefficient: coefficient, exponent: length - position)
+  end
+
+  polynomials.size > 1 ? Polynomial.new(polynomials: polynomials) : polynomials.first
+end
+
+private
+
+def parse_coefficients!(str)
+  str.split("/").map { |segment| Integer(segment) }
 end
