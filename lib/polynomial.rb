35d5547eaea0b0b1e7da1321f224355a45f21999
Lucas Mbiwe <lucas@geek4good.com>
2019-03-27 15:48:21 +0000

    Add error handling to Polynomial
    Default all properties to nil but ensure either polynomials or a coefficient
    along with an exponent are passed on initialisation.

--- lib/polynomial.rb
+++ lib/polynomial.rb
@@ -1,15 +1,19 @@
 class Polynomial
   attr_reader :coefficient, :exponent, :polynomials
 
-  def initialize(coefficient:, exponent:, polynomials: nil)
+  def initialize(coefficient: nil, exponent: nil, polynomials: nil)
     @coefficient = coefficient
     @exponent = exponent
     @polynomials = polynomials
+
+    [coefficient, exponent, polynomials].all?(&:nil?) ||
+      polynomials.nil? && [coefficient, exponent].any?(&:nil?) and
+      fail ArgumentError.new("Either pass polynomials or a coefficient along with an exponent")
   end
 
   def differentiate
     polynomials.nil? ?
       self.class.new(coefficient: coefficient * exponent, exponent: exponent - 1) :
-      self.class.new(coefficient: nil, exponent: nil, polynomials: polynomials.map(&:differentiate))
+      self.class.new(polynomials: polynomials.map(&:differentiate))
   end
 end
--- spec/lib/polynomial_spec.rb
+++ spec/lib/polynomial_spec.rb
@@ -1,32 +1,58 @@
 RSpec.describe Polynomial do
-  subject(:polynomial) { described_class.new(coefficient: coefficient, exponent: exponent) }
-  let(:coefficient) { 7 }
-  let(:exponent) { 3 }
-
   describe "#initialize" do
-    it "assigns the coefficient" do
-      expect(polynomial.coefficient).to eq(coefficient)
+    it "fails by default" do
+      expect { described_class.new }.to raise_error(ArgumentError)
     end
 
-    it "assigns the exponent" do
-      expect(polynomial.exponent).to eq(exponent)
+    context "when coefficient and exponent are given" do
+      subject(:polynomial) { described_class.new(coefficient: coefficient, exponent: exponent) }
+      let(:coefficient) { 7 }
+      let(:exponent) { 3 }
+
+      it "assigns the coefficient" do
+        expect(polynomial.coefficient).to eq(coefficient)
+      end
+
+      it "assigns the exponent" do
+        expect(polynomial.exponent).to eq(exponent)
+      end
+
+      it "defaults to assigning nil to polynomials" do
+        expect(polynomial.polynomials).to be_nil
+      end
     end
 
-    it "defaults to assigning nil to polynomials" do
-      expect(polynomial.polynomials).to be_nil
-    end
+    context "when polynomials are given" do
+      subject(:polynomial) { described_class.new(polynomials: polynomials) }
+      let(:polynomials) { [first_polynomial, second_polynomial] }
 
-    context "when polynomials given" do
-      subject(:polynomial) { described_class.new(coefficient: nil, exponent: nil, polynomials: polynomials) }
-      let(:polynomials) { [] }
+      let(:first_polynomial) { described_class.new(coefficient: first_coefficient, exponent: first_exponent) }
+      let(:first_coefficient) { 4 }
+      let(:first_exponent) { 3 }
+
+      let(:second_polynomial) { described_class.new(coefficient: second_coefficient, exponent: second_exponent) }
+      let(:second_coefficient) { 3 }
+      let(:second_exponent) { 2 }
 
       it "assigns the polynomials" do
         expect(polynomial.polynomials).to eq(polynomials)
+      end
+
+      it "defaults to assigning nil to coefficient" do
+        expect(polynomial.coefficient).to be_nil
+      end
+
+      it "defaults to assigning nil to exponent" do
+        expect(polynomial.exponent).to be_nil
       end
     end
   end
 
   describe "#differenciate" do
+    subject(:polynomial) { described_class.new(coefficient: coefficient, exponent: exponent) }
+    let(:coefficient) { 7 }
+    let(:exponent) { 3 }
+
     it "returns the polynomial's derivative" do
       derivative = polynomial.differentiate
 
@@ -35,7 +61,7 @@ RSpec.describe Polynomial do
     end
 
     context "for complex polynomials" do
-      subject(:polynomial) { described_class.new(coefficient: nil, exponent: nil, polynomials: polynomials) }
+      subject(:polynomial) { described_class.new(polynomials: polynomials) }
       let(:polynomials) { [first_polynomial, second_polynomial] }
 
       let(:first_polynomial) { described_class.new(coefficient: first_coefficient, exponent: first_exponent) }
